66a5bb811fb67ffb6d926731ea4bd7dcb5f9275e
VinnyTheLegend <66708167+VinnyTheLegend@users.noreply.github.com>
2023-11-19 01:57:58 -0500

    more vehicle data

--- sql-fetch/vehicles.go
+++ sql-fetch/vehicles.go
@@ -7,29 +7,36 @@ import (
 type Vehicle struct {
 	CitizenID string
 	Model string
+	Plate string
+	Garage *string
+	Fuel int
+	Engine float64
+	Body float64
+	State int
+	Location *string
 }
 
 func allVehicles() ([]Vehicle, error) {
-		var vehicles []Vehicle
-		rows, err := db.Query("SELECT citizenid, vehicle FROM player_vehicles")
-		if err != nil {
-			return nil, fmt.Errorf("vehicleFetch: %q", err)
-		}
-		defer rows.Close()
-		// Loop through rows, using Scan to assign column data to struct fields.s
-		for rows.Next() {
-			var vehicle Vehicle
-			if err := rows.Scan(&vehicle.CitizenID, &vehicle.Model); err != nil {
-				return nil, fmt.Errorf("vehicleFetch: %q", err)
-			}
-	
-			vehicles = append(vehicles, vehicle)
-		}
-		if err := rows.Err(); err != nil {
+	var vehicles []Vehicle
+	rows, err := db.Query("SELECT citizenid, vehicle, plate, garage, fuel, engine, body, state, location FROM player_vehicles")
+	if err != nil {
+		return nil, fmt.Errorf("vehicleFetch: %q", err)
+	}
+	defer rows.Close()
+	// Loop through rows, using Scan to assign column data to struct fields.s
+	for rows.Next() {
+		var vehicle Vehicle
+		if err := rows.Scan(&vehicle.CitizenID, &vehicle.Model, &vehicle.Plate, &vehicle.Garage, &vehicle.Fuel, &vehicle.Engine, &vehicle.Body, &vehicle.State, &vehicle.Location); err != nil {
 			return nil, fmt.Errorf("vehicleFetch: %q", err)
 		}
 
+		vehicles = append(vehicles, vehicle)
+	}
+	if err := rows.Err(); err != nil {
+		return nil, fmt.Errorf("vehicleFetch: %q", err)
+	}
 
 
-		return vehicles, nil
+
+	return vehicles, nil
 }
